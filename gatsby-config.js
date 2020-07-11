eda55568413f2d28376a9311d99ca398d6dd3950
carlos612r <carlos612r@gmail.com>
2020-07-10 22:42:57 -0400

    agregar gatsby file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    }
+  ],
 }
