eca4089ad919cad6cbe14b07742b48451a7a123c
carlos612r <carlos612r@gmail.com>
2020-07-19 23:41:18 -0400

    gregar configuracion imagen

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,22 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`
       }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200
+            }
+          }
+        ]
+      }
     }
   ],
 }
